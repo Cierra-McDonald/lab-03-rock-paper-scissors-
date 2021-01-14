aa4fb66f971ac799d58f773554aeab0b5a7626cc
Cierra-McDonald <cierralauren15@gmail.com>
2021-01-13 22:15:34 -0700

    **update deleted else if used objects

--- app.js
+++ app.js
@@ -2,45 +2,86 @@
 import { makeMUJ } from './utils.js'; 
 
 const poofButton = document.getElementById('buttonPoof');
-const currentDraw = document.getElementById('current-shoot');
 const totalWins = document.getElementById('wins');
 const totalRounds = document.getElementById('totalPlayed');
 const totalLosses = document.getElementById('losses'); 
+const totalTies = document.getElementById('tie');
+console.log(totalTies);
 
-console.log(poofButton, currentDraw, totalWins, totalRounds, totalLosses);
+console.log(poofButton, totalWins, totalRounds, totalLosses);
 
 // initialize state
 let disneyWins = 0;
-let disneyTotal= 0;
+let disneyTotal = 0;
+let disneyLose = 0;
+let disneyTie = 0; 
+
+const PlayerRules = {
+    'Maleficent' : {
+        'Ursula' : 'win',
+        'Jafar': 'lose',
+        'Maleficent': 'tie'
+    },
+    'Ursula' : {
+        'Maleficent' : 'lose',
+        'Jafar' : 'win',
+        'Ursula': 'tie'
+    }, 
+    'Jafar' : {
+        'Maleficent' : 'wins',
+        'Ursula' : 'lose',
+        'Jafar': 'tie'
+    }
+};
+const playersObject = {
+    0: 'Maleficent',
+    1: 'Ursula', 
+    2: 'Jafar',
+};
 
 // makeMUJ();
 
 poofButton.addEventListener('click', () => {
     // console.log('im in the button');
-    disneyWins++;
-    const computerThrow = Math.round(Math.random());
+    // disneyTotal++;
+    const computerThrow = Math.ceil(Math.random() * 2);
+    // Randomly generates a number between 0 and 2
 
-    const computerMUJ = makeMUJ(computerThrow);
+    let computerPlayer = playersObject[computerThrow];
     // console.log(computerMUJ); 
 
-    const selectedVillianButton = document.querySelector('input[type="radio"]:checked');
-    // console.log(selectedVillianButton);
+    const userPlayer = document.querySelector('input[type="radio"]:checked');
+    // // console.log(selectedVillianButton);
+    const gameOutcome = PlayerRules[userPlayer.value][computerPlayer];
+    // const userVillian = selectedVillianButton.value;
+    // // console.log(userVillian);
+    console.log('the outcome of the game: ', gameOutcome);
 
-    const userVillian = selectedVillianButton.value;
-    // console.log(userVillian);
-  
-    if (userVillian === computerMUJ) {
-        currentDraw.textContent = 'Mirror, mirror on the wall, it is a tie afterall!';
-    } else if (userVillian === 'Maleficent' && computerMUJ === 'Ursula') {
-        disneyWins++,
-        currentDraw.textContent = 'Mistriss of Evil rules them all, You win!';
-    } else if (userVillian === 'Ursula' && computerMUJ === 'Jafar') {
-        disneyWins++,
-        currentDraw.textContent = 'You are the true queen of the sea, You win!';
-    } else if (userVillian === 'Jafar' && computerMUJ === 'Maleficent') {
-        disneyWins++,
-        currentDraw.textContent = 'Prince Ali has nothing on you either, You win!';
+    if (gameOutcome === 'win') {
+        disneyWins++;
+    } else if (gameOutcome === 'lose') {
+        disneyLose++;
+    } else if (gameOutcome === 'tie') {
+        disneyTie++;
     }
+    totalRounds.textContent = disneyWins + disneyLose + disneyTie;
+    totalWins.textContent = disneyWins;
+    totalLosses.textContent = disneyLose;
+    totalTies. textContent = disneyTie;
+
+
+    // if (userVillian === computerMUJ) {
+    //     currentDraw.textContent = 'Mirror, mirror on the wall, it is a tie afterall!';
+    // } else if (userVillian === 'Maleficent' && computerMUJ === 'Ursula') {
+    //     disneyWins++,
+    //     currentDraw.textContent = 'Mistriss of Evil rules them all, You win!';
+    // } else if (userVillian === 'Ursula' && computerMUJ === 'Jafar') {
+    //     disneyWins++,
+    //     currentDraw.textContent = 'You are the true queen of the sea, You win!';
+    // } else if (userVillian === 'Jafar' && computerMUJ === 'Maleficent') {
+    //     disneyWins++,
+    //     currentDraw.textContent = 'Prince Ali has nothing on you either, You win!';
+    // }
 
 });
 
